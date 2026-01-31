237f87ce2953c787c5b8ff2abde85373a1bb96be
Ayush <ayush@airful.io>
2026-01-31 10:23:17 +0530

    deploy test after reconnect

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ export function Hero() {
             </Link>
             <Link href="/case-studies">
               <Button variant="outline" size="lg">
-                View Our Work
+                View Our Worksss
               </Button>
             </Link>
           </motion.div>
